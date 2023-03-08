3eac07f3bfe6d00dfb189eda28587e24153177fc
Serhii Salatin <lifenjoy8888@gmail.com>
2023-03-08 17:41:54 +0200

    initial commit

---
+++ src/main/java/com/salatin/apigatewaycarrepair/ApiGatewayCarRepairApplication.java
@@ -0,0 +1,15 @@
+package com.salatin.apigatewaycarrepair;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class ApiGatewayCarRepairApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayCarRepairApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/salatin/apigatewaycarrepair/ApiGatewayCarRepairApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salatin.apigatewaycarrepair;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayCarRepairApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
