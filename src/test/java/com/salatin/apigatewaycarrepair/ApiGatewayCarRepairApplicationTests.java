50418b8a2afadb317f92245f16a3b02583828722
Serhii Salatin <lifenjoy8888@gmail.com>
2023-03-10 14:02:45 +0200

    params were fixed

--- src/test/java/com/salatin/apigatewaycarrepair/ApiGatewayCarRepairApplicationTests.java
+++ src/test/java/com/salatin/apigatewaycarrepair/ApiGatewayCarRepairApplicationTests.java
@@ -2,6 +2,7 @@ package com.salatin.apigatewaycarrepair;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.oauth2.client.annotation.RegisteredOAuth2AuthorizedClient;
 
 @SpringBootTest
 class ApiGatewayCarRepairApplicationTests {
