1808a279824004e198c3a6b74c9fd872b64c33c3
Serhii Salatin <lifenjoy8888@gmail.com>
2023-05-24 14:27:29 +0300

    SecurityConfig class was created and Csrf disabled

---
+++ src/main/java/com/salatin/apigatewaycarrepair/config/SecurityConfig.java
@@ -0,0 +1,18 @@
+package com.salatin.apigatewaycarrepair.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
+        http
+            .csrf(ServerHttpSecurity.CsrfSpec::disable);
+
+        return http.build();
+    }
+}
